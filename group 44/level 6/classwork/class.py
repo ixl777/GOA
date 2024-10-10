ce8e650995fb8fd5326a9673d50dfda050ae0e79
ixl777 <gobejishviliandria86@gmail.com>
2024-10-10 19:14:59 +0400

    second commit

---
+++ group 44/level 6/classwork/class.py
@@ -0,0 +1,5 @@
+input("Enter your name")
+print(input)
+
+#impilicit არის მონაცემების ხელით შეყვანა ხოლო explicit არის ავტომატურად შეყვანა
+print(input*20)
---
+++ group 44/level 6/homework/level 6.py
@@ -0,0 +1,43 @@
+s = int(input("Enter your age "))
+a = print(float(s+10))
+print(type(a))
+
+last_name=str(input("enter your last name "))
+gamravleba=last_name*100
+print(gamravleba)
+
+Andria="Andria"
+Gobejishvili="Gobejishvili"
+print(Andria+Gobejishvili)
+
+Bmw="Bmw"
+M8="m8"
+print(Bmw+M8)
+
+Amd="amd"
+Rx6600="Rx6600"
+print(Amd+Rx6600)
+
+intel="intel"
+i5="i5"
+print(intel+i5)
+
+Deepcool="deepcool"
+Gamestorm="gamestorm"
+print(Deepcool+Gamestorm)
+
+#Float=ნებისმიერი ათწილადი რიცხვი
+#String=ნებისმიერი მონაცემი მოგცეული ბრჭყალებში
+#integer=ნებისმიერი მთელი რიცხვი
+#Boolean=True/False 
+#True=სიმართლე
+#False=ტყუილი
+
+t="fullbox"
+k=7.77
+J=777
+h=True
+print(type(t))
+print(type(k))
+print(type(J))
+print(type(h))
