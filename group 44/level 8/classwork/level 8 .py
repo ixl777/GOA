6e16a449b01c0d6fa3e80e0eb0fc0bb1aff44978
ixl777 <gobejishviliandria86@gmail.com>
2024-10-24 20:59:32 +0400

    second commit

---
+++ group 44/level 8/classwork/level 8 .py
@@ -0,0 +1,14 @@
+# or ოპერატორის დროს True გამოსახულება იქნება True or True ს რომ დავწერდ ხოლო and ოპერატორის დროს True and false-იც რომ დავწეროთ მაინც იქნება True
+
+#False or False
+#True or True
+#True or False
+#False and False
+#True and True 
+#True and False
+
+s = int(input("Enter your age "))
+Enter_your_age=18
+
+print (s>Enter_your_age)
+
---
+++ group 44/level 8/homework/level 8.py
@@ -0,0 +1,59 @@
+print(3>1)
+print(38>3)
+print(910>190)
+print(820<8200)
+print(777<7777)
+print(888<8888)
+print(888==888)
+print(777==777)
+print(999==999)
+
+m = 777
+s = int(input("enter your fav number "))
+
+
+if m == s:
+    print("you have same fav number as me")
+else:
+    print("you dont have same fav number as me")
+
+
+
+print(True or False and 5 > 3 or "name" == "name" and 123 == "123" and 5 >= 5)
+#True
+
+last_name="Gobejishvili"
+f=int(input("Enter your age"))
+T=str(input("Enter your lastname"))
+if T==last_name:
+    print("you have same last name as me")
+else:
+    print("you dont have same last name as me")
+    
+
+if f>=18:
+    print("you are სრულწლოვანი")
+else:
+    print("you are not სრულწლოვანი")
+
+
+
+
+#რა არის ალგორითმი მ მოქმედებათა ერთობლიობის 
+# ზუსტი და სრული აღწერა, 
+# რომელთა მკაცრად განსაზღვრული 
+# თანმიმდევრობით შესრულება 
+# განაპირობებს დასმული ამოცანის 
+# ამოხსნას.
+
+a = int(input("enter number N1 "))
+b = int(input("enter number N2 "))
+c = int(input("enter number N3 "))
+d = int(input("enter number N4 "))
+e = int(input("enter number N5 "))
+
+sum = a+b+c+d+e
+v = sum/5
+print(v)
+
+
